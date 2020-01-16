81068e2dbf928a076d9b37647cea4a9a64ebcf81
bhaskarpraveen <praveennaidu264@gmail.com>
2020-01-16 15:34:50 +0530

    finalhome

--- assets/normal/scriptregister.js
+++ assets/normal/scriptregister.js
@@ -7,16 +7,7 @@ const signupButton = document.getElementById('signup-button'),
 
 /**
  * Add event listener to the "Sign Up" button
- */
-signupButton.addEventListener('click', () => {
-  userForms.classList.remove('bounceRight')
-  userForms.classList.add('bounceLeft')
-}, false)
+
 
 /**
  * Add event listener to the "Login" button
- */
-loginButton.addEventListener('click', () => {
-  userForms.classList.remove('bounceLeft')
-  userForms.classList.add('bounceRight')
-}, false)
